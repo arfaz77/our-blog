6a25096955b396623e9830334c292f569b179c0e
arfaz77 <amjadalipaduppu@gmail.com>
2024-05-06 15:07:48 +0530

    MERN-BLOG Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { BrowserRouter,Route,Routes } from "react-router-dom"
+import Home from "./pages/Home"
+// import About from "./pages/about"
+import Signup from "./pages/Signup"
+import Signin from "./pages/Signin"
+import Dashboaed from "./pages/Dashboaed"
 
 export default function Main() {
   return (
-    <div className='text-blue-600'> main</div>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            {/* <Route path="/About" element={<About/>}/> */}
+            <Route path="/Sign-in" element={<Signin/>}/>
+            <Route path="/Sign-up" element={<Signup/>}/>
+            <Route path="/Dashboard" element={<Dashboaed/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/Dashboaed.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Dashboaed() {
+  return (
+    <div>Dashboaed</div>
+  )
+}
