6030c715b0200f2f80a3572348746ba8f2d174e0
arfaz77 <amjadalipaduppu@gmail.com>
2024-05-08 11:10:35 +0530

    created a middleware for error handling

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,11 +1,12 @@
 import { error } from "console";
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const  signup=async(req,res)=>{
+export const  signup=async(req,res,next)=>{
 const {username,email,password}=req.body;   
  if(!username||!email||!password ||username==''||email==='' ||password===""){
-    return res.status(400).json({message : 'all field ar required'})
+   next(errorHandler(400,"all fields are required"))
 
  }
 
@@ -19,7 +20,7 @@ const {username,email,password}=req.body;
  try{ await newUser.save()
     res.json('sign up successfull')}
     catch(error){
-        res.status(500).json({message : error.message});
+       next(error);
     }
 
 }
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import { error } from "console";
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(()=>{console.log('db conected');}).catch(err=>{console.log(err)})
@@ -15,3 +16,14 @@ app.listen(3002 ,()=>{
 
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
+
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode||500
+    const message= err.message|| 'internal server error';
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
